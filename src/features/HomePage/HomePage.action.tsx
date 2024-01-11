073385d5c203977b222d3ab9ca22ac7b413e601a
ngmthaq-bglobalcorp <thangnm@bglobalcorp.com>
2024-01-11 18:26:56 +0700

    Update react router dom service

--- src/features/HomePage/HomePage.action.tsx
+++ src/features/HomePage/HomePage.action.tsx
@@ -1,8 +1,12 @@
-import { PostService } from "@/api/services";
 import { ActionFunction, ShouldRevalidateFunction } from "react-router-dom";
+import { PostService } from "@/api/services";
+import { useLoading } from "@/common/hooks";
 
 export const useHomePageAction: ActionFunction = async (ctx) => {
+  const { openAppLoading } = useLoading();
+  openAppLoading(true);
   const formData = await ctx.request.formData();
+  openAppLoading(false);
   return PostService.createPost(formData);
 };
 
--- src/features/HomePage/HomePage.loader.tsx
+++ src/features/HomePage/HomePage.loader.tsx
@@ -1,7 +1,11 @@
 import { LoaderFunction } from "react-router-dom";
 import { PostService } from "@/api/services";
+import { useLoading } from "@/common/hooks";
 
 export const useHomePageLoader: LoaderFunction = async (ctx) => {
+  const { openAppLoading } = useLoading();
+  openAppLoading(true);
   const posts = await PostService.getAllPosts();
+  openAppLoading(false);
   return { posts };
 };
