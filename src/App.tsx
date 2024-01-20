436eeaf2368d5b1c98024cef208b245f9b2c2641
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-01-20 10:17:24 +0700

    Add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./configs/router";
 import { useHandleSnackbar } from "./common/hooks";
 import { AppLoading } from "./common/components/generics";
-import { AppContext, AppContextType } from "./Context";
+import { AppContext, AppContextType } from "./AppContext";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
